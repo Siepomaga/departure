9a89d8432391a783129c6fa087f17b1fbf633351
Krzysztof Michalski <krzysztof.michalski@siepomaga.pl>
2024-10-17 12:58:48 +0200

    Fix for exec_delete and other

--- lib/active_record/connection_adapters/percona_adapter.rb
+++ lib/active_record/connection_adapters/percona_adapter.rb
@@ -127,6 +127,26 @@ module ActiveRecord
         end
       end
 
+      def write_query?(sql) # :nodoc:
+        read_query = ActiveRecord::ConnectionAdapters::AbstractAdapter.build_read_query_regexp(
+          :desc, :describe, :set, :show, :use, :kill
+        )
+
+        !read_query.match?(sql)
+      rescue ArgumentError # Invalid encoding
+        !read_query.match?(sql.b)
+      end
+
+      def exec_delete(sql, name = nil, binds = [])
+        execute(to_sql(sql, binds), name)
+        mysql_adapter.raw_connection.affected_rows
+      end
+      alias exec_update exec_delete
+
+      def exec_insert(sql, name = nil, binds = [], pk = nil, sequence_name = nil, returning: nil) # rubocop:disable Lint/UnusedMethodArgument, Metrics/LineLength, Metrics/ParameterLists
+        execute(to_sql(sql, binds), name)
+      end
+
       def internal_exec_query(sql, name = 'SQL', binds = [], prepare: false, async: false, allow_retry: false) # :nodoc:
         if ActiveRecord::VERSION::MAJOR >= 7 && ActiveRecord::VERSION::MINOR >= 1
           if should_be_run_with_departure?(sql)
@@ -151,76 +171,74 @@ module ActiveRecord
       end
       alias exec_query internal_exec_query
 
-      if ActiveRecord::VERSION::MAJOR >= 7 && ActiveRecord::VERSION::MINOR >= 2
-        # Executes a SELECT query and returns an array of rows. Each row is an
-        # array of field values.
-        def select_rows(arel, name = nil, binds = [])
-          select_all(arel, name, binds).rows
+      # Executes a SELECT query and returns an array of rows. Each row is an
+      # array of field values.
+      def select_rows(arel, name = nil, binds = [])
+        select_all(arel, name, binds).rows
+      end
+
+      # Executes a SELECT query and returns an array of record hashes with the
+      # column names as keys and column values as values.
+      def select(sql, name = nil, binds = [], **kwargs)
+        exec_query(sql, name, binds, **kwargs)
+      end
+
+      # Returns true, as this adapter supports migrations
+      def supports_migrations?
+        true
+      end
+
+      # rubocop:disable Metrics/ParameterLists
+      def new_column(field, default, type_metadata, null, table_name, default_function, collation, comment)
+        Column.new(field, default, type_metadata, null, table_name, default_function, collation, comment)
+      end
+      # rubocop:enable Metrics/ParameterLists
+
+      # Adds a new index to the table
+      #
+      # @param table_name [String, Symbol]
+      # @param column_name [String, Symbol]
+      # @param options [Hash] optional
+      def add_index(table_name, column_name, options = {})
+        if ActiveRecord::VERSION::STRING >= '6.1'
+          index_definition, = add_index_options(table_name, column_name, **options)
+          execute <<-SQL.squish
+            ALTER TABLE #{quote_table_name(index_definition.table)}
+              ADD #{schema_creation.accept(index_definition)}
+          SQL
+        else
+          index_name, index_type, index_columns, index_options = add_index_options(table_name, column_name, **options)
+          execute <<-SQL.squish
+            ALTER TABLE #{quote_table_name(table_name)}
+              ADD #{index_type} INDEX
+              #{quote_column_name(index_name)} (#{index_columns})#{index_options}
+          SQL
+        end
+      end
+
+      # Remove the given index from the table.
+      #
+      # @param table_name [String, Symbol]
+      # @param options [Hash] optional
+      def remove_index(table_name, column_name = nil, **options)
+        if ActiveRecord::VERSION::STRING >= '6.1'
+          return if options[:if_exists] && !index_exists?(table_name, column_name, **options)
+          index_name = index_name_for_remove(table_name, column_name, options)
+        else
+          index_name = index_name_for_remove(table_name, options)
         end
 
-        # Executes a SELECT query and returns an array of record hashes with the
-        # column names as keys and column values as values.
-        def select(sql, name = nil, binds = [], **kwargs)
-          exec_query(sql, name, binds, **kwargs)
-        end
+        execute "ALTER TABLE #{quote_table_name(table_name)} DROP INDEX #{quote_column_name(index_name)}"
+      end
 
-        # Returns true, as this adapter supports migrations
-        def supports_migrations?
-          true
-        end
+      def schema_creation
+        SchemaCreation.new(self)
+      end
 
-        # rubocop:disable Metrics/ParameterLists
-        def new_column(field, default, type_metadata, null, table_name, default_function, collation, comment)
-          Column.new(field, default, type_metadata, null, table_name, default_function, collation, comment)
-        end
-        # rubocop:enable Metrics/ParameterLists
-
-        # Adds a new index to the table
-        #
-        # @param table_name [String, Symbol]
-        # @param column_name [String, Symbol]
-        # @param options [Hash] optional
-        def add_index(table_name, column_name, options = {})
-          if ActiveRecord::VERSION::STRING >= '6.1'
-            index_definition, = add_index_options(table_name, column_name, **options)
-            execute <<-SQL.squish
-              ALTER TABLE #{quote_table_name(index_definition.table)}
-                ADD #{schema_creation.accept(index_definition)}
-            SQL
-          else
-            index_name, index_type, index_columns, index_options = add_index_options(table_name, column_name, **options)
-            execute <<-SQL.squish
-              ALTER TABLE #{quote_table_name(table_name)}
-                ADD #{index_type} INDEX
-                #{quote_column_name(index_name)} (#{index_columns})#{index_options}
-            SQL
-          end
-        end
-
-        # Remove the given index from the table.
-        #
-        # @param table_name [String, Symbol]
-        # @param options [Hash] optional
-        def remove_index(table_name, column_name = nil, **options)
-          if ActiveRecord::VERSION::STRING >= '6.1'
-            return if options[:if_exists] && !index_exists?(table_name, column_name, **options)
-            index_name = index_name_for_remove(table_name, column_name, options)
-          else
-            index_name = index_name_for_remove(table_name, options)
-          end
-
-          execute "ALTER TABLE #{quote_table_name(table_name)} DROP INDEX #{quote_column_name(index_name)}"
-        end
-
-        def schema_creation
-          SchemaCreation.new(self)
-        end
-
-        def change_table(table_name, _options = {})
-          recorder = ActiveRecord::Migration::CommandRecorder.new(self)
-          yield update_table_definition(table_name, recorder)
-          bulk_change_table(table_name, recorder.commands)
-        end
+      def change_table(table_name, _options = {})
+        recorder = ActiveRecord::Migration::CommandRecorder.new(self)
+        yield update_table_definition(table_name, recorder)
+        bulk_change_table(table_name, recorder.commands)
       end
 
       # Returns the MySQL error number from the exception. The
@@ -241,6 +259,10 @@ module ActiveRecord
       # naming of this method.
       def get_full_version # rubocop:disable Style/AccessorMethodName
         mysql_adapter.raw_connection.server_info[:version]
+      end
+
+      def last_inserted_id(result)
+        mysql_adapter.send(:last_inserted_id, result)
       end
 
       private
